d61d38dd77764021e723fa46cb8d665d3ac51a8e
jared <jaredcrace@gmail.com>
2022-10-18 21:31:54 -0500

    checking in argparse

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-print("hello world")
+import argparse
 
-print("hi owen")
+
+def testfunc():
+    
+
+if __name__ == '__main__':
+    description_text = '''
+    Useful script to help configure Ubuntu machines 
+    '''
+
+    example_text = '''
+    some example text goes here
+    '''
+
+    parser = argparse.ArgumentParser(description=description_text,
+                                     epilog=example_text,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-v',
+                        '--verbose',
+                        help='verbose output',
+                        default=None)
+
+    args = parser.parse_args()
+
+    print('here')
+
+    print(args)
+
+
+    print('end')
+
